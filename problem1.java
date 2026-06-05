077310f3959164d7f979cd3747eb34f512ff9e19
kamrankhursheed <khankamran16329@gmail.com>
2026-06-05 19:34:11 +0530

    Add sum of natural numbers solution

---
+++ problem1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class problem1 {
+        public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the  natural number");
+        int n=sc.nextInt();
+        int Sum=n*(n+1)/2;
+        System.out.println("Sum="+Sum);
+    }
+}
