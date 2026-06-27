d7d44d548ba37179ef05d4ad628c9cfe42f8e4d0
kamrankhursheed <khankamran16329@gmail.com>
2026-06-27 18:07:04 +0530

    Create palindromenumber_08.java
    add file

---
+++ day_02/palindromenumber_08.java
@@ -0,0 +1,29 @@
+package day_02;
+
+// Date: 27 June 2025
+// File: palindrome_number
+// Topic: Check if a Number is Palindrome
+
+import java.util.Scanner;
+
+public class palindromenumber_08 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter number: ");
+        int n = sc.nextInt();
+
+        int num = n; // storing original number for later comparison
+        int rev_num = 0;
+        while (n != 0) {
+            rev_num = rev_num * 10 + (n % 10);
+            n /= 10;
+        }
+
+        if (rev_num == num)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+
+        sc.close();
+    }
+}
