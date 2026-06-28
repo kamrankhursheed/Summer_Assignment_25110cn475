b3c51e76d2a59083d4a79129cc0ec5f5805467a9
kamrankhursheed <khankamran16329@gmail.com>
2026-06-28 18:25:46 +0530

    Update perfect_num_function_48.java

--- day_12/perfect_num_function_48.java
+++ day_12/perfect_num_function_48.java
@@ -1,5 +1,22 @@
 package day_12;
 
 public class perfect_num_function_48 {
-    
+
+    static boolean perfectNumber(int n) {
+        int sum = 0;
+        int i = 1;
+        while (i < n) {
+            if (n % i == 0)
+                sum += i;
+            i++;
+        }
+        return sum == n;
+    }
+
+    public static void main(String[] args) {
+
+        int number = 54;
+        System.out.println(perfectNumber(number));
+
+    }
 }
