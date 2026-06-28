1ce282414062f16bb6b0e8f962d28a8e8f791ac7
kamrankhursheed <khankamran16329@gmail.com>
2026-06-28 11:53:44 +0530

    Create sum_function_41.java

---
+++ day_11/sum_function_41.java
@@ -0,0 +1,13 @@
+public class sum_function_41 {
+
+    static int addition(int x, int y) {
+        return (x + y);
+    }
+
+    public static void main(String[] args) {
+
+        int a = 10, b = 7;
+        System.out.println(addition(a, b));
+
+    }
+}
