2294ce03fc8655e8194e122f2680e106c7fba9d6
kamrankhursheed <khankamran16329@gmail.com>
2026-06-28 11:01:15 +0530

    Update nth_fibbonaciterm_14.java

--- day_04/nth_fibbonaciterm_14.java
+++ day_04/nth_fibbonaciterm_14.java
@@ -1,5 +1,28 @@
-package day_04;
+package day_04; 
+    import java.util.Scanner;
 
-public class nth_fibbonaciterm_14 {
-    
+public class nth_fibbonaciterm_14{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the term number: ");
+        int n = sc.nextInt();
+
+        int f1 = 0;
+        int f2 = 1;
+
+        if (n == 1) {
+            System.out.println(f1);
+        } else if (n == 2) {
+            System.out.println(f2);
+        } else {
+            for (int i = 3; i <= n; i++) {
+                int temp = f1;
+                f1 = f2;
+                f2 = temp + f2;
+            }
+            System.out.println(f2);
+        }
+        sc.close();
+    }
 }
+
