e6f6181cb3d295ad6bcf0767faf0a5aca30a15ce
kamrankhursheed <khankamran16329@gmail.com>
2026-06-28 18:19:24 +0530

    Update armstrong_num_function_46.java

--- day_12/armstrong_num_function_46.java
+++ day_12/armstrong_num_function_46.java
@@ -1,5 +1,33 @@
 package day_12;
 
 public class armstrong_num_function_46 {
+                
     
+
+    static int countDigits(int n) {
+        int count = 0;
+        while (n != 0) {
+            n /= 10;
+            count++;
+        }
+        return count;
+    }
+
+    static boolean isArmstrong(int n, int totalDigits) {
+        int newNum = 0;
+        int tempNum = n;
+        while (n != 0) {
+            newNum += (int) Math.pow(n % 10, totalDigits);
+            n /= 10;
+        }
+        return newNum == tempNum;
+    }
+
+    public static void main(String[] args) {
+
+        int number = 153;
+        int digitCount = countDigits(number);
+        System.out.println(isArmstrong(number, digitCount));
+
+    }
 }
