35414ef50cfc7a4d3d66ec7c5fd209232b047f83
kamrankhursheed <khankamran16329@gmail.com>
2026-06-28 11:54:58 +0530

    Create maximum_function_42.java

---
+++ day_11/maximum_function_42.java
@@ -0,0 +1,13 @@
+public class maximum_function_42 {
+
+    static int maximum(int x, int y) {
+        return x > y ? x : y;
+    }
+
+    public static void main(String[] args) {
+
+        int a = 10, b = 25;
+        System.out.println(maximum(a, b));
+
+    }
+}
