3d2a87672934ad9557573e341760981a4101c5b0
kamrankhursheed <khankamran16329@gmail.com>
2026-06-29 23:32:17 +0530

    Create diagonal_sum_76.java

---
+++ day_19/diagonal_sum_76.java
@@ -0,0 +1,20 @@
+package day_19;
+
+public class diagonal_sum_76 {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j || Math.abs(i - j) == 2) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+
+        System.out.println("the diagonal sum is equals to: " + sum);
+    }
+}
