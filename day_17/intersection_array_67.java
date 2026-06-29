633924b319d8514699d47ca4663c0b38b1066f7a
kamrankhursheed <khankamran16329@gmail.com>
2026-06-29 23:20:07 +0530

    Update intersection_array_67.java

---
+++ day_17/intersection_array_67.java
@@ -0,0 +1,39 @@
+package day_17;
+
+import java.util.Scanner;
+
+public class intersection_array_67 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter the size of the two arrays which needs to be intersected: ");
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] array_1 = new int[n];
+        int[] array_2 = new int[m];
+
+        System.out.print("enter the elements of the array1: ");
+        for (int i = 0; i < n; i++) {
+            array_1[i] = sc.nextInt();
+        }
+
+        System.out.print("enter the elements of the array2: ");
+        for (int i = 0; i < m; i++) {
+            array_2[i] = sc.nextInt();
+        }
+
+        System.out.print("intersection array is --> ");
+        for (int i = 0; i < n; i++) {
+            int target = array_1[i];
+            for (int j = 0; j < m; j++) {
+                if (target == array_2[j]) {
+                    System.out.print(target + " ");
+                    break;
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
